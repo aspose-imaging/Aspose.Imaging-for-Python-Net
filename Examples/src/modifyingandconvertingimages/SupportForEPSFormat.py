3c378bc2ad7a42cc1a1219d2efe17909d68d3da3
Evgeniy Sidenko <evgeniy.sidenko@aspose.com>
2023-11-02 22:20:26 +0600

    Updated upto the version 23.10

--- Examples/src/modifyingandconvertingimages/SupportForEPSFormat.py
+++ Examples/src/modifyingandconvertingimages/SupportForEPSFormat.py
@@ -1,7 +1,5 @@
 ﻿import aspose.pycore as aspycore
-from aspose.imaging.imageoptions import PngOptions
-from aspose.imaging.fileformats.eps import *
-from aspose.imaging.fileformats.eps.consts import *
+from aspose.imaging.fileformats.eps import EpsImage
 from aspose.imaging import Image
 import os
 
@@ -24,35 +22,18 @@ if 'get_output_dir' not in dir():
 data_dir = os.path.join(get_data_root_dir(), 'ModifyingAndConvertingImages')
 output_path = os.path.join(get_output_dir(), "anyEpsFile.png")
 print("Running example SupportForEPSFormat")
+
+eps_preview_files = []
 with aspycore.as_of(Image.load(os.path.join(data_dir, "sample.eps")),
 					EpsImage) as eps_image:
-	# check if EPS image has any raster preview to proceed (for now only raster preview is supported)
-	if eps_image.has_raster_preview:
-		if eps_image.photoshop_thumbnail is not None:
-			pass
-
-		if eps_image.eps_type == EpsType.INTERCHANGE:
-			# Get EPS Interchange subformat instance
-			eps_interchange_image = aspycore.as_of(eps_image, EpsInterchangeImage)
-			if eps_interchange_image.raster_preview is not None:
-				print("Has raster_preview")
-				pass
-		else:
-			# Get EPS Binary subformat instance
-			eps_binary_image = aspycore.as_of(eps_image, EpsBinaryImage)
-			if eps_binary_image.tiff_preview is not None:
-				print("Has tiff_preview")
-				pass
-
-			if eps_binary_image.wmf_preview is not None:
-				print("Has wmf_preview")
-				pass
-
-		# export EPS image to PNG (by default, best available quality preview is used for export)
-		eps_image.save(output_path, PngOptions())
+	for preview in eps_image.get_preview_images():
+		previewPath = os.path.join(data_dir, "output." + preview.file_format.name.lower())
+		preview.save(previewPath)
+		eps_preview_files.append(previewPath)
 
 if 'SAVE_OUTPUT' not in os.environ:
-	if os.path.exists(output_path):
-		os.remove(output_path)
+	for output_path in eps_preview_files:
+		if os.path.exists(output_path):
+			os.remove(output_path)
 
 print("Finished example SupportForEPSFormat")
--- Examples/src/modifyingandconvertingimages/eps/ExportEps.py
+++ Examples/src/modifyingandconvertingimages/eps/ExportEps.py
@@ -1,9 +1,6 @@
 ﻿import aspose.pycore as aspycore
-from aspose.imaging import Image, PdfComplianceVersion
-from aspose.imaging.imageoptions import TiffOptions, PdfOptions
-from aspose.imaging.fileformats.tiff.enums import TiffExpectedFormat
-from aspose.imaging.fileformats.eps import EpsImage, EpsPreviewFormat, EpsBinaryImage
-from aspose.imaging.fileformats.pdf import PdfCoreOptions
+from aspose.imaging import Image
+from aspose.imaging.fileformats.eps import EpsImage, EpsPreviewFormat
 import os
 
 
@@ -24,29 +21,14 @@ if 'get_output_dir' not in dir():
 print("Running example ExportEps")
 # The path to the documents directory.
 data_dir = os.path.join(get_data_root_dir(), 'eps')
-out_file1 = os.path.join(get_output_dir(), "Sample.pdf")
-out_file2 = os.path.join(get_output_dir(), "Sample1.tiff")
-out_file3 = os.path.join(get_output_dir(), "Sample2.tiff")
-with aspycore.as_of(Image.load(os.path.join(data_dir, "Sample.eps")), EpsImage) as image:
-	obj_init = PdfCoreOptions()
-	obj_init.pdf_compliance = PdfComplianceVersion.PDF_A1B
-	options = PdfOptions()
-	options.pdf_core_options = obj_init
-	image.preview_to_export = EpsPreviewFormat.POST_SCRIPT_RENDERING
-	image.save(out_file1, options)
+out_file = os.path.join(get_output_dir(), "Sample.tiff")
 
-with aspycore.as_of(Image.load(os.path.join(data_dir, "Sample.eps")), EpsBinaryImage) as image:
-	# Tiff image export options
-	options = TiffOptions(TiffExpectedFormat.TIFF_JPEG_RGB)
-	# The first way:
-	image.tiff_preview.save(out_file2, options)
-	# The second way:
-	image.preview_to_export = EpsPreviewFormat.TIFF
-	image.save(out_file3, options)
+with aspycore.as_of(Image.load(os.path.join(data_dir, "Sample.eps")), EpsImage) as image:
+	tiffPreview = image.get_preview_image(EpsPreviewFormat.TIFF)
+	if tiffPreview is not None:
+		tiffPreview.save(out_file)
 
 if 'SAVE_OUTPUT' not in os.environ:
-	os.remove(out_file1)
-	os.remove(out_file2)
-	os.remove(out_file3)
+	os.remove(out_file)
 
 print("Finished example ExportEps")
